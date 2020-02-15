01255df3a914f6cab98a9d1e2647a4a197f1f05b
mkeneqa <mikeneqa@gmail.com>
2020-02-14 22:39:30 -0700

    import jquery

--- assets/vue/Views/Home.vue
+++ assets/vue/Views/Home.vue
@@ -5,7 +5,34 @@
 </template>
 
 <script>
+    import $ from "jquery";
     export default {
-        name: "Home"
+        name: "Home",
+        data () {
+            return {
+                loading: false,
+                post: null,
+                error: null
+            }
+        },
+        created () {
+            // fetch the data when the view is created and the data is
+            // already being observed
+            this.fetchData()
+        },
+        watch: {
+            // call again the method if the route changes
+            //'$route': 'fetchData'
+        },
+        methods: {
+            fetchData () {
+                $.post('/api/about')
+                    .done(function (data) {
+                        console.log("server data => ", data.data);
+                    });
+
+                //console.log('Data Fetched!')
+            }
+        }
     };
 </script>
